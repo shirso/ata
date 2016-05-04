0ab4c6b2eb2b159cb5326d34ae9b31e676a50bc0
SHIRSO <SHIRSO>
2016-05-04 22:37:37 +0530

    Sidebars

--- includes/sidebar-init.php
+++ includes/sidebar-init.php
@@ -4,49 +4,33 @@ function elegance_widgets_init() {
 
  // Location: at the bottom of the content
      register_sidebar(array(
-        'name'                    => 'Blog Page Sidebar',
-        'id'                         => 'blog_sidebar',
-        'description'   => __( 'Located in the Right Sidebar of Blog Page'),
-        'before_widget' => '<div class="resent-con">',
-        'after_widget' => '</div>',
+        'name'                    =>__('Footer Sidebar 1','ata'),
+        'id'                         => 'footer_sidebar1',
+        'description'   => __( 'Located in the top left of footer section','ata'),
+        'before_widget' => '',
+        'after_widget' => '',
         'before_title' => '<h3>',
         'after_title' => '</h3>',
     ));         
     
     register_sidebar(array(
-        'name'                    => 'Default SideBar',
-        'id'                         => 'likebox',
-        'description'   => __( 'Located in Sidebar of Page'),
-        'before_widget' => '<div class="side-table">',
-        'after_widget' => '</div>',
+        'name'                    => __('Footer Sidebar 2','ata'),
+        'id'                         => 'footer_sidebar2',
+        'description'   => __( 'Located in the bottom left of footer section','ata'),
+        'before_widget' => '',
+        'after_widget' => '',
         'before_title' => '<h3>',
         'after_title' => '</h3>',
-    ));  
-    
- 
-     
-  
-   
-   register_sidebar(array(
-        'name'                    => 'Footer Side Address',
-        'id'                         => 'sidebar',
-        'description'   => __( 'Located in contact page sidebar'),
-        'before_widget' => '',
-        'after_widget' => '',
-        'before_title' => '<h2>',
-        'after_title' => '</h2>',
-    )); 
-    
-    register_sidebar(array(
-        'name'                    => 'Sidebar Contact',
-        'id'                         => 'sidebarcontact',
-        'description'   => __( 'Located in contact page sidebar'),
-        'before_widget' => '',
-        'after_widget' => '',
-        'before_title' => '<h2>',
-        'after_title' => '</h2>',
     ));
-    
+    register_sidebar(array(
+        'name'                    => __('Footer Sidebar 3','ata'),
+        'id'                         => 'footer_sidebar3',
+        'description'   => __( 'Located in the top right of footer section','ata'),
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>',
+    ));
 }
 add_action( 'widgets_init', 'elegance_widgets_init' );
 ?>
